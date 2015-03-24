ba7c38a5be00a4c45881f08bd1242e2895446435
rioshen <rioxshen@gmail.com>
2015-03-24 12:47:39 -0400

    refactor folder structure
    For python, things become easy, each chapter in one single file seems
    fine.

--- python/chap7.py
+++ python/chap7.py
@@ -4,6 +4,19 @@
 Problem 7.2
 Detect cycle in linked list
 '''
+def merge(l1, l2):
+    dummy = ListNode(-1)
+    curr = dummy
+    while l1 and l2:
+        if l1.val < l2.val:
+            curr.next = l1
+            l1 = l1.next
+        else:
+            curr.next = l2
+            l2 = l2.next
+        curr = curr.next
+    curr.next = l1 if l1 else l2
+    return dummy.next
 
 def find_cycle(head):
     if not head or not head.next:
